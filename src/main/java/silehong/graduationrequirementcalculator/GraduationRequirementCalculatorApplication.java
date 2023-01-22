1769bd34333fbad1673afefba9600b0c7fe69388
hong-sile <gurwns9325@naver.com>
2023-01-22 18:03:43 +0900

    feat: init project

---
+++ src/main/java/silehong/graduationrequirementcalculator/GraduationRequirementCalculatorApplication.java
@@ -0,0 +1,13 @@
+package silehong.graduationrequirementcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationRequirementCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationRequirementCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/silehong/graduationrequirementcalculator/GraduationRequirementCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package silehong.graduationrequirementcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationRequirementCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
